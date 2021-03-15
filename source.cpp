9299ae41a3661868272ef2cfea48b2f9e0388a8b
sheraztariq033 <67158851+sheraztariq033@users.noreply.github.com>
2021-03-15 21:26:03 +0500

    i am adding enrollment, name and semester

--- source.cpp
+++ source.cpp
@@ -9,6 +9,7 @@ int main()
 	cout<<"Student who contributed in this repo on 15th march 2021"<<endl;
 	cout << "03-134182-088 Usman Hamid 6th Semester" << endl;
 	cout<<" 03-134182-037 Farhan Ashraf 6th Semester"<< endl;
+
 	
 	
 	/*
@@ -25,7 +26,7 @@ int main()
 	
 	
 	
-	
+	cout<<"03-134202-082 Sheraz Tariq 2nd Semester"<<endl;
 	
 	
 	
